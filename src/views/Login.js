64bba3732d08501c1ba96168941cd8696c83227f
simongodhelper1287 <simongodhelper@gmail.com>
2023-04-14 18:01:17 +0800

    小調

--- src/views/Login.js
+++ src/views/Login.js
@@ -9,16 +9,18 @@ const Login = () => {
     <Layout>
       <div class="bg-white border login_div">
         <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Group className="mb-3 d_b" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
+            <span className="error">安安您哪位?</span>
             <Form.Control type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="mb-3 d_b" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
+            <span className="error">安安您哪位?</span>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
 
